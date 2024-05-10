4228e503df5f2cdf80eb761425c53a3764764688
Abdumajodov Laziz <153080975+Laziz0627@users.noreply.github.com>
2024-05-11 02:19:20 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// let son = Number(prompt("ixtiyoriy son qoying: "))
+
+// let resultl = "";
+
+// if (son % 2 == 0) {
+//     resultl = "juf son";
+//     console.log(resultl)
+// } 
+// else if(son % 2 == 1) {
+//     resultl = "toq son";
+//     console.log(resultl)
+// }
+
+
+// let yosh = prompt("siz necha yoshdasiz? ")
+
+// if (yosh >= 17) {
+//     console.log('yoq')
+// }
+// else {
+//     console.log('hA')
+// }
+
+
+
+// let yosh = prompt('siz nech yoshdasiz ')
+
+// if (yosh >= 6 == yosh < 12)
+//    console.log('kirish mumkin');
+// else if (yosh >= 12 == yosh < 18)
+//    console.log('kirish taqiqlanadi');
+
+
+
+let yosh = prompt('siz necha yoshdasiz')
+
+if(yosh > 18) {
+    console.log('kirish mumkin');
+} else console.log('kirish taqiqlandi');
